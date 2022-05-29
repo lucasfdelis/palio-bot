23acbaf5936c5eb550e689033bbd3c0dbddf2758
Lucas Alves <lucasalvesf78@gmail.com>
2022-05-28 22:18:23 -0300

    Adicionando tempo de pausa na execução

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import tweepy
 from acesso import token
+import time
 auth = tweepy.OAuthHandler(token[0], token[1])
 auth.set_access_token(token[3], token[4])
 api = tweepy.API(auth)
 
-for tweet in api.search_tweets(q="Palio",
-                               lang="pt-BR",
-                               result_type="recent",
-                               count=100):
-    if tweet.user.id_str == "1526710339676545029":
-        pass
-    else:
-        if not tweet.favorited:
+while True:
+    for tweet in api.search_tweets(q="Palio",
+                                   lang="pt",
+                                   result_type="recent",
+                                   count=100):
+        if tweet.user.id_str == "1526710339676545029":
+            pass
+        else:
+            print('Postagem: ', tweet.text)
             try:
                 tweet.favorite()
-            except Exception as e:
-                print(e)
-        if not tweet.retweeted:
+            except:
+                print('O Bot já favoritou essa postagem.')
             try:
                 tweet.retweet()
-            except Exception as e:
-                print(e)
+            except:
+                print('O bot já retweetou essa postagem.')
+                pass
+            print(' ')
+    print('Pausando pesquisa por 5 minutos...')
+    time.sleep(300)
